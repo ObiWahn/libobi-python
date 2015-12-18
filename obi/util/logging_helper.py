906d7867faf2709818f119de92d867664b44607f
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-12-19 00:24:19 +0100

    chage: code is now better readable

--- obi/util/logging_helper.py
+++ obi/util/logging_helper.py
@@ -14,26 +14,28 @@ class APILoggedBase():
     def __getattribute__(self,name):
         if not obi_api_logging_enabled:
             return super().__getattribute__(name)
-        else:
-            do_log = False
-            if (not (name.startswith("__") or name.startswith("_obi_"))):
-                do_log = True
-                log_string="get: {c}.{a}".format(
-                    c = str(self.__class__.__name__), a = name
-                )
-            try:
-                rv = super().__getattribute__(name)
-            except Exception as e:
-                if do_log:
-                    self._obi_logger.error(log_string + " ERROR")
-                raise
+
+        do_log = False
+        if not name.startswith("__") and not name.startswith("_obi_"):
+            do_log = True
+            log_string="get: {c}.{a}".format(
+                c = str(self.__class__.__name__), a = name
+            )
+        try:
+            rv = super().__getattribute__(name)
+        except Exception as e:
             if do_log:
-                self._obi_logger.info(log_string + " -> " + str(rv))
-            return rv
+                self._obi_logger.error(log_string + " ERROR")
+            raise
+        if do_log:
+            self._obi_logger.info(log_string + " -> " + str(rv))
+        return rv
 
     def __setattr__(self,name,value):
-        if (obi_api_logging_enabled and
-            not (name.startswith("__") or name.startswith("_obi_"))):
+        if ( obi_api_logging_enabled and
+             not name.startswith("__") and
+             not name.startswith("_obi_")
+        ):
             self.__class__._obi_logger.info("set: {c}.{a} <- {v}".format(
                 c = str(self.__class__.__name__), a = name, v = value
             ))
