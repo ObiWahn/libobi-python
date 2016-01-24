48831b291ff58c0f302f47b990d7bafe694e2898
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-24 11:30:44 +0100

    add: args to string function

---
+++ obi/util/function_details.py
@@ -0,0 +1,7 @@
+def args_to_str(*args, **kwargs):
+    rv = ""
+    for arg in args:
+        rv += "{0}, ".format(str(arg))
+    for key, val in kwargs:
+        rv += "{0} = {1}, ".format(key,str(val))
+    return rv.rstrip(', ')
